8145cfb30e588cc38dfa33392ad4b835ad6ab74c
byurch <byurchak@gmail.com>
2016-08-24 17:21:00 +0300

    Example with blue balls

--- MoveBall/Program.cs
+++ MoveBall/Program.cs
@@ -11,11 +11,51 @@ namespace MoveBall
 {
     class Program
     {
+        public static List<WebSocket> sockets;
         static void Main(string[] args)
         {
-            using (var server = new WebSocketEventListener(new IPEndPoint(IPAddress.Any, 8009), new WebSocketListenerOptions() { SubProtocols = new String[] { "123456" }, NegotiationTimeout = TimeSpan.FromSeconds(30) }))
+            sockets = new List<WebSocket>();
+            using (var server = new WebSocketEventListener(new IPEndPoint(IPAddress.Any, 8001), new WebSocketListenerOptions() { SubProtocols = new String[] { "123456" }, NegotiationTimeout = TimeSpan.FromSeconds(30) }))
             {
-
+				SetEvents(server);
+                server.Start();
+                Console.ReadKey(true);
+                Console.ReadKey(true);
             }
+        }
+
+        public static void SetEvents(WebSocketEventListener server)
+        {
+            server.OnConnect += (ws) => {
+                sockets.Add(ws);
+                Console.WriteLine("Connection from " + ws.RemoteEndpoint.ToString());
+            };
+            server.OnDisconnect += (ws) =>
+            {
+                sockets.Remove(ws);
+                Console.WriteLine("Disconnection from " + ws.RemoteEndpoint.ToString());
+            };
+            server.OnError += (ws, ex) =>
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            };
+            server.OnMessage += (ws, msg) =>
+            {
+                Console.WriteLine("Message from [" + ws.RemoteEndpoint + "]: " + msg);
+                var wsContext = ws;
+                //var task = Task.Factory.StartNew(() =>
+                //{
+                    var util = new Utils();
+                    // util.ProcessMessage(wsContext);
+                    foreach (var w in sockets)
+                    {
+                        if (w != ws)
+                        {
+                            w.WriteString(msg);
+                        }
+                    }
+                //});
+            };
+        }
     }
 }
